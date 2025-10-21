9b27f206ac5b3d1c47118df9eb6622a07429e6e9
Lincoln Wallace <lincoln.wallace@canonical.com>
2025-10-21 13:48:55 -0300

    refact: print to stderr and move variable assigns
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- chat.py
+++ chat.py
@@ -8,23 +8,27 @@ def main():
     data = {}
     path = ""
     jsonPath = ""
+    url = ""
     try:
         path = os.environ["SNAP_DATA"]
         print(f"SNAP_DATA found: {path}")
     except KeyError:
-        print("Error: not running in a snap environment.")
+        print("Error: not running in a snap environment.", file=sys.stderr)
         sys.exit(1)
 
     jsonPath = path + "/share/endpoints/endpoints.json"
     try:
         with open(jsonPath) as f:
             data = json.load(f)
+        print(f"json data: {data}")
+        url = data["openai"]
     except FileNotFoundError:
-        print(f"Error: connection.json file not found in {jsonPath}")
+        print(
+            f"Error: connection.json file not found in {jsonPath}, using default endpoint.",
+            file=sys.stderr,
+        )
         sys.exit(1)
 
-    print(f"json data: {data}")
-    url = data["openai"]
     print(f"Using OpenAI endpoint: {url}")
     client = OpenAI(base_url=url, api_key="dummy_key")
     response = client.chat.completions.create(
